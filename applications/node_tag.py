c16c26a5042932a3d72dce8eca2a2b0d3bb7253e
bilin.wang <wangbilin@orientsoft.cn>
2020-01-14 15:09:31 +0800

    update node_tag

--- applications/node_tag.py
+++ applications/node_tag.py
@@ -39,50 +39,6 @@ class NodeTagAction(Resource):
             return 'ERROR', 500
         return 'success', 200
 
-    # def get(self):
-    #     # 根据tag查询node节点
-    #     # 只传入tag_id不传入node_id
-    #     from models.node import NodesHasTag, Node
-    #     # 参数均为列表形式，传入id
-    #     tag = [int(x) for x in request.args.get('tag').split(',')] if request.args.get('tag') else None
-    #     # 若同时存在node和tag的筛选条件，则不能传多个条件
-    #     # if node and tag and (len(node) > 1 and len(tag) > 1):
-    #     #     return '违规操作', 400
-    #     # 若无tag传入时，可保证无错误产生
-    #     node_tag_list = NodesHasTag.query
-    #     data_return = []
-    #     # 特殊情况已排除，直接遍历筛除即可
-    #     # if node:
-    #     #     node_tag_list = node_tag_list.filter(NodesHasTag.nodes_id.in_(node))
-    #     if tag:
-    #         node_tag_list = NodesHasTag.query.filter(NodesHasTag.tag_id.in_(tag))
-    #     else:
-    #         return '无标签传入', 400
-    #     node_dict = {}
-    #     node_list = []
-    #     result = node_tag_list.all()
-    #     # 组装形式为{node_id_1: [tag_id_1, tag_id_2, tag_id_3], node_id_2: [tag_id_2]}的字典
-    #     for x in result:
-    #         if not node_dict.get(x.nodes_id):
-    #             node_dict[x.nodes_id] = [x.tag_id]
-    #         else:
-    #             if x.tag_id not in node_dict[x.nodes_id]:
-    #                 node_dict[x.nodes_id].append(x.tag_id)
-    #     # 筛选出符合要求的node_id列表
-    #     for key, value in node_dict.items():
-    #         if set(tag) <= set(value):
-    #             node_list.append(key)
-    #     nodes = Node.query.filter(Node.id.in_(node_list))
-    #     # 展开数据
-    #     for x in nodes:
-    #         data_return.append({
-    #             'id': x.id,
-    #             'name': x.name,
-    #             'arch': x.arch_class.name,
-    #             'parallel': x.parallel,
-    #             'online': x.online
-    #         })
-    #     return jsonify(data_return)
 
     # def delete(self):
     #     from models.node import NodesHasTag
@@ -100,28 +56,3 @@ class NodeTagAction(Resource):
     #         db.session.commit()
     #     return 'success', 200
 
-#
-# class TagNodeAction(Resource):
-#     def get(self):
-#         # 根据node查询tag
-#         from models.node import NodesHasTag
-#         from models.tag import Tag
-#         node = request.args.get('node')
-#         tag_node_list = NodesHasTag.query
-#         if node:
-#             # for x in node:
-#             tag_node_list = tag_node_list.filter_by(nodes_id=node)
-#         tag_list = []
-#         result = tag_node_list.all()
-#         for x in result:
-#             tag_list.append(x.tag_id)
-#         tag_list = list(set(tag_list))
-#         tags = Tag.query.filter(Tag.id.in_(tag_list))
-#         data_return = []
-#         for x in tags:
-#             data_return.append({
-#                 'id': x.id,
-#                 'name': x.name,
-#                 'type': x.type
-#             })
-#         return jsonify(data_return)
