ac42b523fcd46b716c470149398c05d1a3c45f68
bilin.wang <wangbilin@orientsoft.cn>
2020-01-14 13:56:28 +0800

    tag 接口修改

--- applications/tag_manage.py
+++ applications/tag_manage.py
@@ -2,6 +2,7 @@
 from flask import request
 from flask_restful import Resource
 from ext import role_check
+import json
 
 
 class TagAction(Resource):
@@ -35,7 +36,12 @@ class TagAction(Resource):
             returnObj.append({
                 "id": str(r.id),
                 "name": r.name,
-                "type": r.type
+                "type": r.type,
+                "nodes": list(map(lambda x: {'id': x.id, 'name': x.name, 'online': x.online, 'parallel': x.parallel,
+                                             'arch': x.arch_class.name}, r.nodes)),
+                "services": list(map(
+                    lambda x: {'id': x.id, 'name': x.name, 'description': x.description, 'image': x.image,
+                               'kubernetes': json.loads(x.kubernetes)}, r.services))
             })
         return returnObj
 
@@ -47,11 +53,9 @@ class TagAction(Resource):
             t = Tag.query.get(id)
             if t:
                 name = request.json.get('name', None)
-                type = request.json.get('type', None)
                 if name:
                     t.name = name
-                if type:
-                    t.type = type
+                # IMPORT IMPORT IMPORT type 不可变更
                 db.session.commit()
             else:
                 return '标签不存在', 400
@@ -74,5 +78,5 @@ class TagAction(Resource):
         except Exception as e:
             print(e)
             db.session.rollback()
-            return 'ERROR', 500
+            return '标签已被使用，无法删除', 400
         return 'success', 200
