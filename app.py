3e6af1f3af7e323aeebe01d5731b91a8c6207ab0
bilin.wang <wangbilin@orientsoft.cn>
2020-02-17 10:28:17 +0800

    1

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ api.add_resource(NodeDeployLink, '/deploy')
 def test(name):
     from applications.common.k8s import get_pod_status, create_node, create_device_model, get_node_status, \
         list_node_status
-    print(get_pod_status(name))
-    return ''
+    return get_pod_status(name), 200
 
 
 @app.before_request
